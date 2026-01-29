2cfd8a5a9a578523db6ea17a98ee8fc39703a066
sl3632 <sl3632@cornell.edu>
2026-01-29 18:07:38 -0500

    Revert "finished implementation"
    This reverts commit edfa353e6e30ada56782b7b9e9493e4773f230a4.

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -19,18 +19,10 @@ module PairTripleDetector_GL
   // Implement pair/triple detector using explicit gate-level modeling
   //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-  wire w;
-  wire y;
-  wire x;
-
-  and(x, in0, in1);
-  or(w, in0, in1);
-  and(y, w, in2);
-  or(out, x, y);
-  // `ECE2300_UNUSED( in0 );
-  // `ECE2300_UNUSED( in1 );
-  // `ECE2300_UNUSED( in2 );
-  // `ECE2300_UNDRIVEN( out );
+  `ECE2300_UNUSED( in0 );
+  `ECE2300_UNUSED( in1 );
+  `ECE2300_UNUSED( in2 );
+  `ECE2300_UNDRIVEN( out );
 
 endmodule
 
